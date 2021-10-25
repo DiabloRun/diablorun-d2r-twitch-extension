329eafbd67b843ae7a7331c8af56a4ff10f4f974
Indrek <leede.indrek@gmail.com>
2021-10-25 11:02:05 +0300

    Built item swap, Twitch user integration

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const inventory = document.getElementById("inventory");
-const user = "Indrek";
+const primaryLeft = document.getElementById("primary_left_slot");
+const primaryRight = document.getElementById("primary_right_slot");
+const secondaryLeft = document.getElementById("secondary_left_slot");
+const secondaryRight = document.getElementById("secondary_right_slot");
+const swapImage = document.getElementById("swap");
+let channelID = "Indrek";
 const itemSlots = [
   "head",
   "amulet",
@@ -15,14 +20,18 @@ const itemSlots = [
   "secondary_right",
 ];
 
-function toggle() {
-  inventory.classList.toggle("visible");
+window.Twitch.ext.onAuthorized(function (auth) {
+  channelID = auth.channelId;
+});
 
-  if (inventory.classList.contains("visible")) {
+function toggle() {
+  inventory.classList.toggle("hidden");
+
+  if (!inventory.classList.contains("hidden")) {
     for (let slot of itemSlots) {
       document.getElementById(slot).src =
         "https://api.diablo.run/d2r/" +
-        user +
+        channelID +
         "/item/character/" +
         slot +
         ".jpg" +
@@ -30,7 +39,7 @@ function toggle() {
         Date.now();
       document.getElementById(slot + "_description").src =
         "https://api.diablo.run/d2r/" +
-        user +
+        channelID +
         "/item-description/character/" +
         slot +
         ".jpg" +
@@ -38,4 +47,12 @@ function toggle() {
         Date.now();
     }
   }
+}
+
+function swap() {
+  primaryLeft.classList.toggle("hidden");
+  primaryRight.classList.toggle("hidden");
+  secondaryLeft.classList.toggle("hidden");
+  secondaryRight.classList.toggle("hidden");
+  swapImage.classList.toggle("secondary");
 }
