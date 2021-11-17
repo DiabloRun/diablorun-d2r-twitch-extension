7d7949e6211f3604c4bc5c670b733d5ea70ec1ff
Indrek <leede.indrek@gmail.com>
2021-11-17 12:00:25 +0200

    Fixed bugs

--- main.js
+++ main.js
@@ -18,76 +18,72 @@ window.Twitch.ext.onAuthorized(function (auth) {
   channelID = auth.channelId;
 });
 
-document.body.addEventListener("load", () => {
-  const inventory = document.getElementById("inventory");
-  const primaryLeft = document.getElementById("primary_left_slot");
-  const primaryRight = document.getElementById("primary_right_slot");
-  const secondaryLeft = document.getElementById("secondary_left_slot");
-  const secondaryRight = document.getElementById("secondary_right_slot");
-  const swapImage = document.getElementById("swap");
+const inventory = document.getElementById("inventory");
+const primaryLeft = document.getElementById("primary_left_slot");
+const primaryRight = document.getElementById("primary_right_slot");
+const secondaryLeft = document.getElementById("secondary_left_slot");
+const secondaryRight = document.getElementById("secondary_right_slot");
+const swapImage = document.getElementById("swap");
 
-  document.getElementById("toggle").addEventListener("click", (event) => {
-    toggle();
-  });
+document.getElementById("toggle").addEventListener("click", (event) => {
+  toggle();
+});
 
-  document.getElementById("toggle").addEventListener("dblclick", (event) => {
+document.getElementById("toggle").addEventListener("dblclick", (event) => {
+  event.stopPropagation();
+});
+
+document
+  .getElementById("swapBtnPrimary")
+  .addEventListener("dblclick", (event) => {
     event.stopPropagation();
   });
 
-  document
-    .getElementById("swapBtnPrimary")
-    .addEventListener("dblclick", (event) => {
-      event.stopPropagation();
-    });
+document
+  .getElementById("swapBtnSecondary")
+  .addEventListener("dblclick", (event) => {
+    event.stopPropagation();
+  });
 
-  document
-    .getElementById("swapBtnSecondary")
-    .addEventListener("dblclick", (event) => {
-      event.stopPropagation();
-    });
+document.getElementById("swapBtnPrimary").addEventListener("click", (event) => {
+  swap();
+});
 
-  document
-    .getElementById("swapBtnPrimary")
-    .addEventListener("click", (event) => {
-      swap();
-    });
+document
+  .getElementById("swapBtnSecondary")
+  .addEventListener("click", (event) => {
+    swap();
+  });
 
-  document
-    .getElementById("swapBtnSecondary")
-    .addEventListener("click", (event) => {
-      swap();
-    });
+function toggle() {
+  inventory.classList.toggle("hidden");
 
-  function toggle() {
-    inventory.classList.toggle("hidden");
-
-    if (!inventory.classList.contains("hidden")) {
-      for (let slot of itemSlots) {
-        document.getElementById(slot).src =
-          "https://api.diablo.run/d2r/" +
-          channelID +
-          "/item/character/" +
-          slot +
-          ".jpg" +
-          "?" +
-          Date.now();
-        document.getElementById(slot + "_description").src =
-          "https://api.diablo.run/d2r/" +
-          channelID +
-          "/item-description/character/" +
-          slot +
-          ".jpg" +
-          "?" +
-          Date.now();
-      }
+  if (!inventory.classList.contains("hidden")) {
+    for (let slot of itemSlots) {
+      document.getElementById(slot).src =
+        "https://api.diablo.run/d2r/" +
+        channelID +
+        "/item/character/" +
+        slot +
+        ".jpg" +
+        "?" +
+        Date.now();
+      document.getElementById(slot + "_description").src =
+        "https://api.diablo.run/d2r/" +
+        channelID +
+        "/item-description/character/" +
+        slot +
+        ".jpg" +
+        "?" +
+        Date.now();
     }
   }
+}
 
-  function swap() {
-    primaryLeft.classList.toggle("hidden");
-    primaryRight.classList.toggle("hidden");
-    secondaryLeft.classList.toggle("hidden");
-    secondaryRight.classList.toggle("hidden");
-    swapImage.classList.toggle("secondary");
-  }
-});
+function swap() {
+  primaryLeft.classList.toggle("hidden");
+  primaryRight.classList.toggle("hidden");
+  secondaryLeft.classList.toggle("hidden");
+  secondaryRight.classList.toggle("hidden");
+  swapImage.classList.toggle("secondary");
+}
